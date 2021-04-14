3e441d9f998724a20678c3aa5753c9113deaeddb
Nenad Jovanovic <shonsi1994@gmail.com>
2021-04-15 00:10:16 +0200

    Contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -46,5 +46,54 @@ $(document).ready(function(){
     }
     
     
+    //form validator
+  
+ if($('.contact-form').length > 0){
+    
+    $('.contact-form').validate({
+        
+        highlight: function(element){ //sluzi za validiranje polja, da li korisnik dobro uneo ZELENO ili pogresno CRVENO
+            $(element).addClass('is-invalid').removeClass('is-valid');
+        },
+        unhighlight:function(element){
+            $(element).addClass('is-valid').removeClass('is-invalid');
+        },
+        //parameteri za svako polje 
+        rules:{  //je objekat gde definisemo za svako polje ono sta treba od uslova da ispuni 
+            name:{
+                required:true, //polje je obavezno
+                minlength: 3 //mora da ima minimum 3 karaktera
+            },
+            email:{
+                required:true, //polje je obavezno
+                email:true //da ga validiramo
+            },
+            message:{
+                required:true
+            }
+        },
+        messages:{ //trebalo bi da se ispise poruka za svako polje, ali ovde smo definisali samo za prvo
+            name:{
+                required:'The Name* field is required' //poruka za prvo polje
+               // minlength: 'The Name* field shold be min 3 characters long'  //poruka za prvo polje
+            },
+            email:{
+                required:'The Email* field is required',
+                email:'Please provide a valid email address'
+            },
+            message:{
+                required:'The Message* field is required'
+            }
+        },
+        
+        //sledece trebamo da odredimo element u kome zelimo da ispisujemo nase greske
+        errorElement: 'p', //po difoltu je label tag, ali smo stavili da bude paragraph
+          errorPlacement: function(error, element) { //sledece definisemo gde ce poruke da se ispisuju po difoltu poruke se ispisuju ispod svakog input polja
+        error.appendTo( element.closest(".form-group").find(".error-msg") );
+  }
+});
+    
+}
+
 });
 
